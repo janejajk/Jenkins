3e3bd1520f14c4ecd9fc31e214abbe340fc77d2e
janejajk <157297222+janejajk@users.noreply.github.com>
2024-03-04 15:04:26 +0530

    Update javaf.java

--- javaf.java
+++ javaf.java
@@ -1,7 +1,7 @@
 class first{
 public static void main(String args[]){
 	System.out.println("Janeja is my name");
-	System.out.println("Changes has been made");
+	System.out.println("Changes 2 has been made");
 
 }
 }
